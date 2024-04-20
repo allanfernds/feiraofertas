d90873625757c38504bb90e5b033f0302f698ec5
Alan Fernandes <allanfernds@gmail.com>
2024-04-20 08:34:12 -0300

    feat: criando os tipos

---
+++ src/app/lib/types.ts
@@ -0,0 +1,22 @@
+export type Category = {
+  id: number;
+  title: string;
+  slug: string;
+};
+
+export type Product = {
+  id: number;
+  title: string;
+  slug: string;
+  price: number;
+  freight: number;
+  discountPrice: number;
+  description: string;
+  imageURL: string;
+  categoryID?: number;
+  saleURL: string;
+  companyID?: number;
+  createdAt?: string;
+  updatedAt?: string;
+  expirationDate?: string;
+};
