9a0ce9f4a0dd8617076130b8aff611865d28764a
Alan Fernandes <allanfernds@gmail.com>
2024-04-21 03:08:02 -0300

    feat: criando card de produtos

--- src/app/ui/CardProduct.tsx
+++ src/app/ui/CardProduct.tsx
@@ -1,32 +1,37 @@
 import Image from 'next/image';
 import { Product } from '../lib/types';
+import Link from 'next/link';
 
 const CardProduct = ({ ...product }: Product) => {
   return (
-    <div className="card w-[350px] md:w-[300px] max-h-[550px] bg-base-100 shadow-xl">
-      <figure>
-        <Image
-          width={400}
-          height={275}
-          src={product.imageURL}
-          alt={product.title}
-          className="object-cover h-[310px] md:h-[310px]"
-        />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{product.title}</h2>
-        <p>{product.description}</p>
-        <div className="border-b border-custom-grey-3/20 h-1 w-full"></div>
-        <p className="w-24 line-through">{product.price}</p>
-        <h2 className="card-title">
-          R$ {product.discountPrice ? product.discountPrice : '00,00'}
-        </h2>
-        <span className="inline text-xs text-emerald-500 font-medium h-1 m-0">
-          {'Frete ' +
-            (product.freight > 0 ? 'R$ ' + product.freight + ',00' : 'Grátis')}
-        </span>
+    <Link href={`/productpage/${product.slug}`}>
+      <div className="card w-[350px] md:w-[300px] max-h-[550px] bg-base-100 shadow-xl">
+        <figure>
+          <Image
+            width={400}
+            height={275}
+            src={product.imageURL}
+            alt={product.title}
+            className="object-cover h-[310px] md:h-[310px]"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{product.title}</h2>
+          <p>{product.description}</p>
+          <div className="border-b border-custom-grey-3/20 h-1 w-full"></div>
+          <p className="w-24 line-through">{product.price}</p>
+          <h2 className="card-title">
+            R$ {product.discountPrice ? product.discountPrice : '00,00'}
+          </h2>
+          <span className="inline text-xs text-emerald-500 font-medium h-1 m-0">
+            {'Frete ' +
+              (product.freight > 0
+                ? 'R$ ' + product.freight + ',00'
+                : 'Grátis')}
+          </span>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
