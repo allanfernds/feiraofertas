50b4ea49abf5e1032500813fa9019f7275e17047
Alan Fernandes <allanfernds@gmail.com>
2024-04-27 18:33:15 -0300

    style: mudando o input pro tipo search com lupa no final

--- src/app/ui/NavBar.tsx
+++ src/app/ui/NavBar.tsx
@@ -12,11 +12,10 @@ export const NavBar = () => {
             </Link>
           </div>
           <div className="form-control">
-            <input
-              type="text"
-              placeholder="Pesquise por produtos, categorias, marca ou loja"
-              className="input input-bordered h-9 w-[95vw] text-sm md:w-96"
-            />
+            <label className="input input-bordered flex items-center h-9 w-[95vw] text-sm md:w-96">
+              <input type="text" className="grow" placeholder="Pesquise por produto, categoria, marca ou loja" />
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+            </label>
           </div>
         </div>
       </div>
